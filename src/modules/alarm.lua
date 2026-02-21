245f5ab1af735f7da99cc0d428fb7bab108f4e13
Adem Kurt <kurtadem2008@gmail.com>
2026-02-21 21:32:58 +0300

    Use sys.File for alarm sound

--- src/modules/alarm.lua
+++ src/modules/alarm.lua
@@ -3,7 +3,7 @@ local audio = require "audio"
 alarm = Object({})
 
 function alarm:constructor(sound_path)
-    self.sound = audio.Sound(sound_path or "assets/sound/alarm.mp3")
+    self.sound = audio.Sound(sys.File(sound_path or "assets/sound/alarm.mp3"))
     self.sound.loop = true
 end
 
